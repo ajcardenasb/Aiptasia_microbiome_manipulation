d3b0c108bcefb00dbfd290e11e629e8831f190a4
ajcardenasb <ajcardenasb1@gmail.com>
2019-10-31 10:59:58 +0100

    Contamination and outlier removal
    Remove OTUs present majoritarily in negetive controls and sample outliers.  Produce normalize and raw new abundance matrices

--- Contaminat_removal.R
+++ Contaminat_removal.R
@@ -36,3 +36,52 @@ write.table(OTU.noConta.r[,c(1:53,56:107)], "OTU_noContaminants.normalized.txt",
 # Exclude contaminants from taxa file
 tax.noConta=subset(as.data.frame(tax), !tax$OTU %in% rownames(Conta)) 
 write.table(tax.noConta, "Tax_noContaminants.txt",  quote = FALSE, row.names=FALSE, sep = "\t") 
+
+
+###########################################
+#####Identifying and removing outliers#####
+###########################################
+
+library("phyloseq")
+library(gridExtra)
+library(plyr)
+
+otu = read.table("OTU_noContaminants.raw.txt", header = TRUE,  row.names=1)
+tax = read.table("Tax_noContaminants.txt", header = TRUE,  row.names=1)
+met = read.table("transplants_metadata.txt", header = TRUE,  row.names=1)
+
+tax.1=tax[,-1]
+otu.t= otu_table(otu, taxa_are_rows=TRUE)
+sam.t= sample_data(met)
+tax.t= tax_table(as.matrix(tax.1))
+
+phy.all= phyloseq(otu.t, tax.t,  sam.t)
+
+subset=subset_samples(phy.all, names(otu) %like% "APO_C_")#6
+subset=subset_samples(phy.all, names(otu) %like% "SYM_C_")#1 or 3
+subset=subset_samples(phy.all, names(otu) %like% "APO_Aip_7D")# 4 or 5
+subset=subset_samples(phy.all, names(otu) %like% "SYM_Aip_7D_")#1
+subset=subset_samples(phy.all, names(otu) %like% "APO_CB_7D_")#6,2 or 4
+subset=subset_samples(phy.all, names(otu) %like% "SYM_CB_7D_")#4
+subset=subset_samples(phy.all, names(otu) %like% "APO_CA_7D_")#2
+subset=subset_samples(phy.all, names(otu) %like% "SYM_CA_7D_")#5
+subset=subset_samples(phy.all, names(otu) %like% "APO_AB_")#2
+subset=subset_samples(phy.all, names(otu) %like% "SYM_AB_")#1
+
+
+PCOA = ordinate(subset, method = "PCoA", distance = "bray") 
+plot_ordination(subset, PCOA ,  label="Replicate", )  + geom_text(mapping = aes(label = Replicate), size = 5) 
+
+outliers=c("APO_C_6", "SYM_C_1","APO_Aip_7D_5", "SYM_Aip_7D_1", "APO_CB_7D_6", "SYM_CB_7D_4", "APO_CA_7D_2", "SYM_CA_7D_5", "APO_AB_2", "SYM_AB_1")
+
+# Exclude outliers from otu and met file
+otu.noOut=otu[, !names(otu) %in% outliers]
+write.table(otu.noOut, "otu_noConta_noOutliers.txt",  quote = FALSE, row.names=TRUE, sep = "\t") 
+met=read.table("transplants_metadata.txt", header = TRUE)
+met.noOut=subset(met, !met$Sample %in% outliers)
+write.table(met.noOut, "transplants_metadata_noOutliers.txt",  quote = FALSE, row.names=FALSE, sep = "\t") 
+
+
+otu2 = read.table("OTU_noContaminants.normalized.txt", header = TRUE,  row.names=1)
+otu2.noOut=otu2[, !names(otu2) %in% outliers]
+write.table(otu2.noOut, "otu_normalized_noConta_noOutliers.txt",  quote = FALSE, row.names=TRUE, sep = "\t") 
